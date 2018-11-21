6ccd15b04933140d71456243bbac599e80f754e4
Roman Romanovski <roman.romanovski@effective-soft.com>
2018-11-21 18:34:19 +0300

    Initial commit

---
+++ react-calendar.js
@@ -0,0 +1,6 @@
+export { default as Calendar } from './src/components/Calendar';
+export { default as Month } from './src/components/Month';
+export { default as Week } from './src/components/Week';
+export { default as Day } from './src/components/Day';
+export { default as util } from './src/utils/util';
+export { default as dateUtils } from './src/utils/dateUtils';
